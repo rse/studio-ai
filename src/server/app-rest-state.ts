361307967a10d3f2390fa0a35bfbf023d4fd2cb6
Ralf S. Engelschall <rse@engelschall.com>
2024-10-03 19:47:31 +0200

    add dynamically adjusted config values (based on process environment variables)

--- src/server/app-rest-state.ts
+++ src/server/app-rest-state.ts
@@ -8,6 +8,7 @@ import path                from "node:path"
 import fs                  from "node:fs"
 import jsYAML              from "js-yaml"
 import ducky               from "ducky"
+import clone               from "clone"
 import * as HAPI           from "@hapi/hapi"
 
 import Argv                from "./app-argv"
@@ -33,19 +34,49 @@ export default class RESTState {
         /*  determine state file  */
         this.stateFile = path.join(this.argv.stateDir, "canvas-ai-state.yaml")
 
+        /*  dynamically adjust configuration  */
+        const adjustConfig = (prefix: string, obj: any, reverse = false) => {
+            if (typeof obj === "object") {
+                for (const field of Object.keys(obj))
+                    obj[field] = adjustConfig(prefix, obj[field], reverse)
+            }
+            else if (typeof obj === "string") {
+                if (reverse) {
+                    /*  map variable value to variable reference  */
+                    for (const name of Object.keys(process.env)) {
+                        if (name.match(new RegExp(`^${prefix}`))) {
+                            const value = process.env[name]
+                            obj = obj.replaceAll(value, `@{${name}}`)
+                        }
+                    }
+                }
+                else {
+                    /*  map variable reference to variable value  */
+                    obj = obj.replaceAll(new RegExp(`@\\{(${prefix}[A-Z0-9_]+)\\}`, "g"), (m, str) => {
+                        if (typeof process.env[str] === "string")
+                            return process.env[str]
+                        else
+                            return m
+                    })
+                }
+            }
+            return obj
+        }
+
         /*  load current state  */
         this.rest.server!.route({
             method: "GET",
             path: "/state",
             handler: async (req: HAPI.Request, h: HAPI.ResponseToolkit) => {
                 return this.db.transaction(Transaction.READ, 4000, async () => {
-                    const state = StateDefault
+                    let state = clone(StateDefault)
                     if (await (fs.promises.stat(this.stateFile).then(() => true).catch(() => false))) {
                         const txt = await this.db.readFile(this.stateFile)
                         const obj = jsYAML.load(txt) as StateType
                         if (ducky.validate(obj, StateSchema))
                             StateUtil.copy(state, obj)
                     }
+                    state = adjustConfig("STUDIOAI_", state)
                     return h.response(state).code(200)
                 })
             }
@@ -67,7 +98,8 @@ export default class RESTState {
             },
             handler: async (req: HAPI.Request, h: HAPI.ResponseToolkit) => {
                 return this.db.transaction(Transaction.WRITE, 4000, async () => {
-                    const state = req.payload as StateType
+                    let state = req.payload as StateType
+                    state = adjustConfig("STUDIOAI_", state, true)
                     const txt = jsYAML.dump(state, { indent: 4, quotingType: "\"" })
                     await this.db.writeFile(this.stateFile, txt)
                     this.restWS.notifyState(state)
@@ -92,7 +124,7 @@ export default class RESTState {
             },
             handler: async (req: HAPI.Request, h: HAPI.ResponseToolkit) => {
                 return this.db.transaction(Transaction.WRITE, 4000, async () => {
-                    const state = StateDefault
+                    let state = clone(StateDefault)
                     if (await (fs.promises.stat(this.stateFile).then(() => true).catch(() => false))) {
                         const txt = await this.db.readFile(this.stateFile)
                         const obj = jsYAML.load(txt) as StateType
@@ -101,6 +133,7 @@ export default class RESTState {
                     }
                     const statePatch = req.payload as StateTypePartial
                     StateUtil.copy(state, statePatch)
+                    state = adjustConfig("STUDIOAI_", state, true)
                     const txt = jsYAML.dump(state, { indent: 4, quotingType: "\"" })
                     await this.db.writeFile(this.stateFile, txt)
                     this.restWS.notifyState(statePatch)
